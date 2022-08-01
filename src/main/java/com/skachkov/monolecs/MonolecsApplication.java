09c25830077f8c8733ebef9ddb01c31481f28fff
hamster4n <oleksii.skachkov@gmail.com>
2022-08-01 11:19:57 +0300

    init commit

---
+++ src/main/java/com/skachkov/monolecs/MonolecsApplication.java
@@ -0,0 +1,13 @@
+package com.skachkov.monolecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skachkov/monolecs/MonolecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skachkov.monolecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolecsApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
